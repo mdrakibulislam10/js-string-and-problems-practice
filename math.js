c3c093d917ed06cbfefae70b66f0191b6090279c
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-02 16:26:09 +0600

    Math-methods: Math.pow abs round ceil floor random max min PI

---
+++ math.js
@@ -0,0 +1,45 @@
+const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+if (gap < 5) {
+    // console.log("you guys can be friend");
+}
+else {
+    // console.log("you guys stay apart");
+}
+
+//
+
+const number = 2.5;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+
+const result2 = Math.ceil(2.1);
+// console.log(result2);
+
+const result3 = Math.floor(5.8);
+// console.log(result3);
+
+// random
+// const random = Math.round(Math.random() * 100);
+// console.log(random);
+
+for (let i = 0; i < 20; i++) {
+    const random = Math.round(Math.random() * 6);
+    // console.log(random);
+};
+
+const a = 4;
+const b = 9;
+const c = 5;
+
+console.log(Math.max(a, b, c));
+// console.log(Math.max(23, 45, 16));
+// console.log(Math.min(45, 67, 12));
+
+// console.log(Math.PI);
