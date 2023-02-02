3b5c1c9dec36122dfba980b79817a7b83b1a613e
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-02 17:58:43 +0600

    get max num

---
+++ max3.js
@@ -0,0 +1,13 @@
+const jim = 69;
+const dela = 97;
+const chinku = 97;
+
+if (jim > dela && jim > chinku) {
+    console.log("jim will get the cake");
+}
+else if (dela > jim && dela > chinku) {
+    console.log("dela will get the cake");
+}
+else {
+    console.log("chinku will get the cake");
+}
