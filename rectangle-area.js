547df746fb8f4ffa34def198e3f0965261a1755c
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-03 11:49:22 +0600

    rectangle-area practice problem

---
+++ rectangle-area.js
@@ -0,0 +1,9 @@
+function rectangleArea(width, length) {
+    const area = width * length;
+    return area;
+};
+
+const width = 30;
+const length = 60;
+const area = rectangleArea(width, length);
+console.log(area);
