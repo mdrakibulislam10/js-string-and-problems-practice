f5c827e7d58f22e75d1274974e311b76203f11a7
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-03 13:37:56 +0600

    find-sec-largest-num-in-an-array

---
+++ second-largest-num-an-arr.js
@@ -0,0 +1,19 @@
+function secondLargest(numbers) {
+    let largest = numbers[0];
+    let secLargest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        if (number > largest) {
+            secLargest = largest;
+            largest = number;
+        }
+        else if (number > secLargest && number < largest) {
+            secLargest = number;
+        }
+    }
+    return secLargest;
+};
+
+const myNumbers = [3, 5, 45, 2, 18, 9];
+const secLarge = secondLargest(myNumbers);
+console.log(secLarge);
